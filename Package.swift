a8ab2e9a46848fed9a4df66296c4eadb7e991e73
GitName <delirious_egging690@silomails.com>
2026-02-24 02:48:42 +0700

    fix: remove broken GPL remnants from Package.swift

--- Package.swift
+++ Package.swift
@@ -49,27 +49,6 @@ let package = Package(
                 .linkedLibrary("c++"),
             ]
         ),
-            ],
-            path: "Sources/_MPVKit-GPL",
-            linkerSettings: [
-                .linkedFramework("AVFoundation"),
-                .linkedFramework("CoreAudio"),
-            ]
-        ),
-                .linkedFramework("CoreVideo"),
-                .linkedFramework("CoreFoundation"),
-                .linkedFramework("CoreMedia"),
-                .linkedFramework("Metal"),
-                .linkedFramework("VideoToolbox"),
-                .linkedLibrary("bz2"),
-                .linkedLibrary("iconv"),
-                .linkedLibrary("expat"),
-                .linkedLibrary("resolv"),
-                .linkedLibrary("xml2"),
-                .linkedLibrary("z"),
-                .linkedLibrary("c++"),
-            ]
-        ),
         //AUTO_GENERATE_TARGETS_BEGIN//
 
         .binaryTarget(
